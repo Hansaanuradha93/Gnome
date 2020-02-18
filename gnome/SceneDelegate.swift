adefacc4585e77a03cd15fa461e8f178cc0aec7d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-02-18 12:14:22 +0530

    Tab asserts added

--- gnome/SceneDelegate.swift
+++ gnome/SceneDelegate.swift
@@ -18,40 +18,40 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     fileprivate func createExploreNC() -> UINavigationController {
         let exploreVC = ExploreViewController.create(viewModel: ExploreViewModel())
         exploreVC.title = "Explore"
-        exploreVC.tabBarItem     = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
+        exploreVC.tabBarItem = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
         
         return UINavigationController(rootViewController: exploreVC)
     }
     
     
     fileprivate func createTrendingNC() -> UINavigationController {
-        let trendingVC             = TrendingViewController.create(viewModel: TrendingViewModel())
-        trendingVC.title           = "Trending"
-        trendingVC.tabBarItem      = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        let trendingVC = TrendingViewController.create(viewModel: TrendingViewModel())
+        trendingVC.title = "Trending"
+        trendingVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
         
         return UINavigationController(rootViewController: trendingVC)
     }
     
     fileprivate func createSearchNC() -> UINavigationController {
-        let searchVC             = SearchViewController.create(viewModel: SearchViewModel())
-        searchVC.title           = "Search"
-        searchVC.tabBarItem      = UITabBarItem(tabBarSystemItem: .search, tag: 2)
+        let searchVC = SearchViewController.create(viewModel: SearchViewModel())
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
         
         return UINavigationController(rootViewController: searchVC)
     }
     
     fileprivate func createLibraryNC() -> UINavigationController {
-        let libraryVC             = LibraryViewController.create(viewModel: LibraryViewModel())
-        libraryVC.title           = "Library"
-        libraryVC.tabBarItem      = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 4)
+        let libraryVC = LibraryViewController.create(viewModel: LibraryViewModel())
+        libraryVC.title = "Library"
+        libraryVC.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 4)
         
         return UINavigationController(rootViewController: libraryVC)
     }
     
     fileprivate func createSettingsNC() -> UINavigationController {
-        let settingsVC             = SettingsViewController.create(viewModel: SettingsViewModel())
-        settingsVC.title           = "Settings"
-        settingsVC.tabBarItem      = UITabBarItem(tabBarSystemItem: .history, tag: 5)
+        let settingsVC = SettingsViewController.create(viewModel: SettingsViewModel())
+        settingsVC.title = "Settings"
+        settingsVC.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 5)
         
         return UINavigationController(rootViewController: settingsVC)
     }
