ffb0a7a913b4ded0f48f76be2e6d6566348683b0
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-02-21 09:15:51 +0530

    Tab bar created

--- gnome/SceneDelegate.swift
+++ gnome/SceneDelegate.swift
@@ -12,14 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.rootViewController = createTabBarController()
         window?.makeKeyAndVisible()
-        configureNavigationBar()
     }
     
     fileprivate func createExploreNC() -> UINavigationController {
         let exploreVC = ExploreViewController.create(viewModel: ExploreViewModel())
         exploreVC.title = "Explore"
-        exploreVC.tabBarItem = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
-        
+        exploreVC.tabBarItem = UITabBarItem(title: "Explore", image: UIImage(named: "explore"), tag: 0)
         return UINavigationController(rootViewController: exploreVC)
     }
     
@@ -27,45 +25,41 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     fileprivate func createTrendingNC() -> UINavigationController {
         let trendingVC = TrendingViewController.create(viewModel: TrendingViewModel())
         trendingVC.title = "Trending"
-        trendingVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        
+        trendingVC.tabBarItem = UITabBarItem(title: "Trending", image: UIImage(named: "trending"), tag: 1)
         return UINavigationController(rootViewController: trendingVC)
     }
     
     fileprivate func createSearchNC() -> UINavigationController {
         let searchVC = SearchViewController.create(viewModel: SearchViewModel())
         searchVC.title = "Search"
-        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
-        
+        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(named: "search"), tag: 2)
         return UINavigationController(rootViewController: searchVC)
     }
     
     fileprivate func createLibraryNC() -> UINavigationController {
         let libraryVC = LibraryViewController.create(viewModel: LibraryViewModel())
         libraryVC.title = "Library"
-        libraryVC.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 4)
-        
+        libraryVC.tabBarItem = UITabBarItem(title: "Library", image: UIImage(named: "library"), tag: 3)
         return UINavigationController(rootViewController: libraryVC)
     }
     
     fileprivate func createSettingsNC() -> UINavigationController {
         let settingsVC = SettingsViewController.create(viewModel: SettingsViewModel())
         settingsVC.title = "Settings"
-        settingsVC.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 5)
-        
+        settingsVC.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "settings"), tag: 4)
         return UINavigationController(rootViewController: settingsVC)
     }
     
     
     fileprivate func createTabBarController() -> UITabBarController {
         let tabBar = UITabBarController()
-        UITabBar.appearance().tintColor = .systemGreen
+        UITabBar.appearance().tintColor = UIColor.red
         tabBar.viewControllers = [createExploreNC(), createTrendingNC(), createSearchNC(), createLibraryNC(), createSettingsNC()]
         return tabBar
     }
     
     fileprivate func configureNavigationBar() {
-        UINavigationBar.appearance().tintColor = .systemGreen
+        UINavigationBar.appearance().tintColor = UIColor.red
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
