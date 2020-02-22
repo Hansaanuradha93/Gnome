134756e86231d9262322aac3746922e1d7c17ff4
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-02-22 23:10:02 +0530

    Code refactored

--- gnome/SceneDelegate.swift
+++ gnome/SceneDelegate.swift
@@ -4,7 +4,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
@@ -13,6 +12,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = createTabBarController()
         window?.makeKeyAndVisible()
     }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+
+
+}
+
+// MARK: - Fileprivate methods
+extension SceneDelegate {
     
     fileprivate func createExploreNC() -> UINavigationController {
         let exploreVC = ExploreViewController.create(viewModel: ExploreViewModel())
@@ -62,16 +77,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         UINavigationBar.appearance().tintColor = UIColor.red
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {}
-
-    func sceneDidBecomeActive(_ scene: UIScene) {}
-
-    func sceneWillResignActive(_ scene: UIScene) {}
-
-    func sceneWillEnterForeground(_ scene: UIScene) {}
-
-    func sceneDidEnterBackground(_ scene: UIScene) {}
-
-
 }
-
