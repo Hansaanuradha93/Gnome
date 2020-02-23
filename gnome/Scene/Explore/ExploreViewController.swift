ae144877da18635a2852622b93b9022889ecdaad
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-02-23 18:50:23 +0530

    Recommended for you cell up and runnig

--- gnome/Scene/Explore/ExploreViewController.swift
+++ gnome/Scene/Explore/ExploreViewController.swift
@@ -12,6 +12,7 @@ class ExploreViewController: UIViewController {
     
     struct Storybaord {
         static let RecentlyPlayedReusableCell =  "RecentlyPlayedReusableCell"
+        static let RecommendedForYouReusableCell = "RecommendedForYouReusableCell"
     }
     
     
@@ -58,6 +59,7 @@ extension ExploreViewController {
     
     fileprivate func registerCells() {
         mainTableView.register(UINib(nibName: String(describing: RecentlyPlayedTableViewCell.self), bundle: Bundle.main), forCellReuseIdentifier: Storybaord.RecentlyPlayedReusableCell)
+        mainTableView.register(UINib(nibName: String(describing: RecommendedForYouTableViewCell.self), bundle: Bundle.main), forCellReuseIdentifier: Storybaord.RecommendedForYouReusableCell)
     }
 }
 
@@ -66,17 +68,25 @@ extension ExploreViewController {
 extension ExploreViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Storybaord.RecentlyPlayedReusableCell, for: indexPath) as! RecentlyPlayedTableViewCell
-        return cell
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: Storybaord.RecentlyPlayedReusableCell, for: indexPath) as! RecentlyPlayedTableViewCell
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: Storybaord.RecommendedForYouReusableCell, for: indexPath) as! RecommendedForYouTableViewCell
+            return cell
+        default:
+            return UITableViewCell()
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 0 || indexPath.row == 3{
-            return 182.0
+            return 185.0
         } else {
             return UITableView.automaticDimension
         }
