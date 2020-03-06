0e176e2d05217fbe3e4a8f28a097247985e2488f
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-06 10:02:47 +0530

    Collection view initial configurations

--- gnome/Scene/Explore/Rewamp/ExploreRewampViewController.swift
+++ gnome/Scene/Explore/Rewamp/ExploreRewampViewController.swift
@@ -17,21 +17,64 @@ class ExploreRewampViewController: UIViewController {
         return viewController!
     }
     
+    struct Storybaord {
+        static let RecentlyPlayedReusableCell =  "RecentlyPlayedReusableCell"
+    }
+    
     
     // MARK: - IBLoutlets
     
-    
+    @IBOutlet weak var collectionView: UICollectionView!
+
     
 
     // MARK: - View Controller
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        configureNavigationBar()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        setupCollectionView()
     }
     
 }
 
 // MARK: - Fileprivate methods
 extension ExploreRewampViewController {
+    
+    fileprivate func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: String(describing: SongCollectionViewCell.self), bundle: Bundle.main), forCellWithReuseIdentifier: Storybaord.RecentlyPlayedReusableCell)
+    }
+    
+    fileprivate func configureNavigationBar() {
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+}
+
+// MARK: - collection view datasource, delegate
+extension ExploreRewampViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storybaord.RecentlyPlayedReusableCell, for: indexPath) as! SongCollectionViewCell
+        return cell
+    }
+    
+    
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ExploreRewampViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 115, height: 155)
+    }
     
 }
