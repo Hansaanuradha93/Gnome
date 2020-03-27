91df145e8cffe6599faf0582a92fb20f10e5e425
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-27 23:01:58 +0530

    removed unneccessary files

--- gnome/Scene/Explore/ExploreViewController.swift
+++ gnome/Scene/Explore/ExploreViewController.swift
@@ -32,8 +32,6 @@ class ExploreViewController: UIViewController {
     // MARK: - ViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        registerCells()
-        configureTableView()
     }
     
     
@@ -47,68 +45,7 @@ class ExploreViewController: UIViewController {
 // MARK: - Fileprivate methods
 extension ExploreViewController {
     
-    fileprivate func configureTableView() {
-        mainTableView.dataSource = self
-        mainTableView.delegate = self
-    }
-    
-    
     fileprivate func configureNavigationBar() {
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
-    
-    
-    fileprivate func registerCells() {
-        mainTableView.register(UINib(nibName: String(describing: RecentlyPlayedTableViewCell.self), bundle: Bundle.main), forCellReuseIdentifier: Storybaord.RecentlyPlayedReusableCell)
-        mainTableView.register(UINib(nibName: String(describing: RecommendedForYouTableViewCell.self), bundle: Bundle.main), forCellReuseIdentifier: Storybaord.RecommendedForYouReusableCell)
-        mainTableView.register(UINib(nibName: String(describing: GetInspiredTableViewCell.self), bundle: Bundle.main), forCellReuseIdentifier: Storybaord.getInspiredReusableCell)
-    }
 }
-
-
-// MARK: - TableView
-extension ExploreViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.row {
-        case 0:
-            let cell = tableView.dequeueReusableCell(withIdentifier: Storybaord.RecentlyPlayedReusableCell, for: indexPath) as! RecentlyPlayedTableViewCell
-            cell.backgroundColor = .red
-            return cell
-        case 1:
-            let cell = tableView.dequeueReusableCell(withIdentifier: Storybaord.RecommendedForYouReusableCell, for: indexPath) as! RecommendedForYouTableViewCell
-            cell.backgroundColor = .blue
-            return cell
-        case 2:
-            let cell = tableView.dequeueReusableCell(withIdentifier: Storybaord.getInspiredReusableCell, for: indexPath) as! GetInspiredTableViewCell
-            cell.backgroundColor = .systemPink
-            return cell
-        case 3:
-            let cell = tableView.dequeueReusableCell(withIdentifier: Storybaord.RecentlyPlayedReusableCell, for: indexPath) as! RecentlyPlayedTableViewCell
-            cell.backgroundColor = .systemTeal
-            return cell
-        default:
-            return UITableViewCell()
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        switch indexPath.row {
-        case 0:
-            return 185.0
-        case 1:
-            return 310.0
-        case 2:
-            return 304.0
-        case 3:
-            return 185.0
-        default:
-            return UITableView.automaticDimension
-        }
-    }
-}
-
