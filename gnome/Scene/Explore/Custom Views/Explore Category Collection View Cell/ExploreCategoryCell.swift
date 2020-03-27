2642019cdc24234f4b785852435710d5e4af6d72
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-27 23:22:06 +0530

    Category collection view outlets added

--- gnome/Scene/Explore/Custom Views/Explore Category Collection View Cell/ExploreCategoryCell.swift
+++ gnome/Scene/Explore/Custom Views/Explore Category Collection View Cell/ExploreCategoryCell.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class ExploreCategoryCell: UICollectionViewCell {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Nib
     override func awakeFromNib() {
         super.awakeFromNib()
     }
