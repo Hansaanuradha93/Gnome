d6c46f38d61c9a390a0d1c33ec461bf7d5589926
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-28 13:56:21 +0530

    Custom tab bar implemented

---
+++ gnome/CommonViews/Tab Bar/GnomeTabBar.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+class GnomeTabBar: UITabBarController {
+
+    // MARK: - View Controller
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        #warning("Need to get this colour from the asserts")
+        UITabBar.appearance().tintColor = .systemRed
+        viewControllers                 = [createExploreNC(), createTrendingNC(), createSearchNC(), createLibraryNC(), createSettingsNC()]
+    }
+}
+
+
+// MARK: - Methods
+extension GnomeTabBar {
+    
+    private func createExploreNC() -> UINavigationController {
+        let exploreVC = ExploreVC()
+        exploreVC.title = TabBar.explore
+        exploreVC.tabBarItem = UITabBarItem(title: TabBar.explore, image: Asserts.explore, tag: 0)
+        return UINavigationController(rootViewController: exploreVC)
+    }
+    
+    
+    private func createTrendingNC() -> UINavigationController {
+        let trendingVC = TrendingVC()
+        trendingVC.title = TabBar.trending
+        trendingVC.tabBarItem = UITabBarItem(title: TabBar.trending, image: Asserts.trending, tag: 1)
+        return UINavigationController(rootViewController: trendingVC)
+    }
+    
+    
+    private func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = TabBar.search
+        searchVC.tabBarItem = UITabBarItem(title: TabBar.search, image: Asserts.search, tag: 2)
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    private func createLibraryNC() -> UINavigationController {
+        let libraryVC = LibraryVC()
+        libraryVC.title = TabBar.library
+        libraryVC.tabBarItem = UITabBarItem(title: TabBar.library, image: Asserts.library, tag: 3)
+        return UINavigationController(rootViewController: libraryVC)
+    }
+    
+    
+    private func createSettingsNC() -> UINavigationController {
+        let settingsVC = SettingsVC()
+        settingsVC.title = TabBar.settings
+        settingsVC.tabBarItem = UITabBarItem(title: TabBar.settings, image: Asserts.settings, tag: 4)
+        return UINavigationController(rootViewController: settingsVC)
+    }
+}
+
--- gnome/SceneDelegate.swift
+++ gnome/SceneDelegate.swift
@@ -9,7 +9,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = createTabBarController()
+        window?.rootViewController = GnomeTabBar()
         window?.makeKeyAndVisible()
     }
 
@@ -29,54 +29,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 // MARK: - Fileprivate methods
 extension SceneDelegate {
-    
-    fileprivate func createExploreNC() -> UINavigationController {
-        let exploreVC = ExploreVC()
-        exploreVC.title = TabBar.explore
-        exploreVC.tabBarItem = UITabBarItem(title: TabBar.explore, image: Asserts.explore, tag: 0)
-        return UINavigationController(rootViewController: exploreVC)
-    }
-    
-    
-    fileprivate func createTrendingNC() -> UINavigationController {
-        let trendingVC = TrendingVC()
-        trendingVC.title = TabBar.trending
-        trendingVC.tabBarItem = UITabBarItem(title: TabBar.trending, image: Asserts.trending, tag: 1)
-        return UINavigationController(rootViewController: trendingVC)
-    }
-    
-    
-    fileprivate func createSearchNC() -> UINavigationController {
-        let searchVC = SearchVC()
-        searchVC.title = TabBar.search
-        searchVC.tabBarItem = UITabBarItem(title: TabBar.search, image: Asserts.search, tag: 2)
-        return UINavigationController(rootViewController: searchVC)
-    }
-    
-    
-    fileprivate func createLibraryNC() -> UINavigationController {
-        let libraryVC = LibraryVC()
-        libraryVC.title = TabBar.library
-        libraryVC.tabBarItem = UITabBarItem(title: TabBar.library, image: Asserts.library, tag: 3)
-        return UINavigationController(rootViewController: libraryVC)
-    }
-    
-    
-    fileprivate func createSettingsNC() -> UINavigationController {
-        let settingsVC = SettingsVC()
-        settingsVC.title = TabBar.settings
-        settingsVC.tabBarItem = UITabBarItem(title: TabBar.settings, image: Asserts.settings, tag: 4)
-        return UINavigationController(rootViewController: settingsVC)
-    }
-    
-    
-    fileprivate func createTabBarController() -> UITabBarController {
-        let tabBar = UITabBarController()
-        UITabBar.appearance().tintColor = UIColor.red
-        tabBar.viewControllers = [createExploreNC(), createTrendingNC(), createSearchNC(), createLibraryNC(), createSettingsNC()]
-        return tabBar
-    }
-    
     
     fileprivate func configureNavigationBar() {
         UINavigationBar.appearance().tintColor = UIColor.red        
