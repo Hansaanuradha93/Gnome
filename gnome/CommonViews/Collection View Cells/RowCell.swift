d5d284b784fd9214bd2c20c650379f0fdf6dc1bb
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-29 11:53:48 +0530

    collection view configured

--- gnome/CommonViews/Collection View Cells/RowCell.swift
+++ gnome/CommonViews/Collection View Cells/RowCell.swift
@@ -2,4 +2,30 @@ import UIKit
 
 class RowCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    static let reuseID      = "RowCell"
+    private let imageView   = GNThumbnaiImageView(frame: .zero)
+    
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
 }
--- gnome/Scene/Explore/ExploreVC.swift
+++ gnome/Scene/Explore/ExploreVC.swift
@@ -4,6 +4,7 @@ class ExploreVC: UIViewController {
 
     
     private let titleLabel = GNTitleLabel(fontSize: 34)
+    private let collectionViewContainer = UIView()
     private let flowLayout = UICollectionViewFlowLayout()
     private var collectionView: UICollectionView!
     
@@ -42,14 +43,18 @@ extension ExploreVC {
             titleLabel.heightAnchor.constraint(equalToConstant: 34)
         ])
     }
+
     
     private func configureCollectionView() {
-        flowLayout.scrollDirection  = .vertical
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
-        view.addSubview(collectionView)
+        flowLayout.scrollDirection      = .vertical
+        collectionView                  = UICollectionView(frame: collectionViewContainer.bounds, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor  = .systemBackground
+        collectionView.dataSource       = self
+        collectionView.delegate         = self
         
-        collectionView.dataSource   = self
-//        collectionView.delegate     = self
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(RowCell.self, forCellWithReuseIdentifier: RowCell.reuseID)
 
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
@@ -60,15 +65,35 @@ extension ExploreVC {
     }
 }
 
+
+
 extension ExploreVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell z/÷÷÷
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RowCell.reuseID, for: indexPath) as! RowCell
+        return cell
     }
     
     
     
 }
+
+
+extension ExploreVC: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Item selected")
+    }
+}
+
+
+
+extension ExploreVC: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
