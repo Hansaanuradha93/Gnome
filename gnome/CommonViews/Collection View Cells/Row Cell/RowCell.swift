5b6d8d4caca1dca219fddf928a428f75d08e8553
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-31 23:26:28 +0530

    collection view configured in RowCell

--- gnome/CommonViews/Collection View Cells/Row Cell/RowCell.swift
+++ gnome/CommonViews/Collection View Cells/Row Cell/RowCell.swift
@@ -14,14 +14,14 @@ class RowCell: UICollectionViewCell {
     // MARK: - Properties
     static let reuseID          = "RowCell"
     private let titleLabel      = GNTitleLabel(fontSize: 22)
-    private let collectionViewContainer = UIView()
     private var collectionView  : UICollectionView!
     
     
     // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+        configureTitleLabel()
+        configureCollectionView()
     }
     
     
@@ -35,21 +35,41 @@ class RowCell: UICollectionViewCell {
     }
     
     
-    private func configure() {
+    private func configureTitleLabel() {
         addSubview(titleLabel)
-        addSubview(collectionViewContainer)
-        collectionViewContainer.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
             titleLabel.heightAnchor.constraint(equalToConstant: 22),
-            
-            collectionViewContainer.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 18),
-            collectionViewContainer.leadingAnchor.constraint(equalTo: leadingAnchor),
-            collectionViewContainer.trailingAnchor.constraint(equalTo: trailingAnchor),
-            collectionViewContainer.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
+    }
+    
+    
+    private func configureCollectionView() {
+        
+        collectionView                  = UICollectionView(frame: .zero, collectionViewLayout: createFlowLayout())
+        collectionView.backgroundColor  = .systemBackground
+        
+        
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 18),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    
+    private func createFlowLayout() -> UICollectionViewFlowLayout {
+        let flowLayout                      = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection          = .horizontal
+        flowLayout.sectionInset             = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        flowLayout.minimumInteritemSpacing  = 12.7
+        return flowLayout
     }
 }
