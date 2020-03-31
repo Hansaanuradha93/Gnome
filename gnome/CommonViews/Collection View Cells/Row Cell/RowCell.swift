4eb32e0a6b612940e5d755d089c61fa7386b82c0
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-31 23:07:23 +0530

    Added collection view container to RowCell

--- gnome/CommonViews/Collection View Cells/Row Cell/RowCell.swift
+++ gnome/CommonViews/Collection View Cells/Row Cell/RowCell.swift
@@ -14,7 +14,7 @@ class RowCell: UICollectionViewCell {
     // MARK: - Properties
     static let reuseID          = "RowCell"
     private let titleLabel      = GNTitleLabel(fontSize: 22)
-    private let flowLayout      = UICollectionViewFlowLayout()
+    private let collectionViewContainer = UIView()
     private var collectionView  : UICollectionView!
     
     
@@ -37,12 +37,19 @@ class RowCell: UICollectionViewCell {
     
     private func configure() {
         addSubview(titleLabel)
+        addSubview(collectionViewContainer)
+        collectionViewContainer.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
-            titleLabel.heightAnchor.constraint(equalToConstant: 22)
+            titleLabel.heightAnchor.constraint(equalToConstant: 22),
+            
+            collectionViewContainer.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 18),
+            collectionViewContainer.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionViewContainer.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionViewContainer.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
 }
