69d7683099fe486e410e65b6c943ef73362ca7ad
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-01 09:22:30 +0530

    LargeSongCell initial configurations done

--- gnome/CommonViews/Collection View Cells/Large Song Cell/LargeSongCell.swift
+++ gnome/CommonViews/Collection View Cells/Large Song Cell/LargeSongCell.swift
@@ -2,4 +2,22 @@ import UIKit
 
 class LargeSongCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    static let reuseID = "LargeSongCell"
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        
+    }
 }
