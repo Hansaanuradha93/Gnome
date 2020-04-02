909128ef6c6439473eabb4d406e779063146e95b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-02 08:42:10 +0530

    Album Cell designed

--- gnome/CommonViews/Collection View Cells/Album Cell/AlbumCell.swift
+++ gnome/CommonViews/Collection View Cells/Album Cell/AlbumCell.swift
@@ -2,4 +2,38 @@ import UIKit
 
 class AlbumCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    private let thumbnailImageView  = GNThumbnaiImageView(frame: .zero)
+    private let titleLabel          = GNSecondaryTitleLabel(fontSize: 17, alignment: .center)
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Methods
+    private func configure() {
+        addSubview(thumbnailImageView)
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            thumbnailImageView.topAnchor.constraint(equalTo: topAnchor),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            thumbnailImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            thumbnailImageView.heightAnchor.constraint(equalTo: thumbnailImageView.widthAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+
+        ])
+    }
 }
