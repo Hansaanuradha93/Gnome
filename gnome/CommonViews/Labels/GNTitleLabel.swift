b992438b8e0cfacc89c79a51b3a23f2abb7f25c8
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-02 08:52:41 +0530

    Added font colour to GTTitleLAbel

--- gnome/CommonViews/Labels/GNTitleLabel.swift
+++ gnome/CommonViews/Labels/GNTitleLabel.swift
@@ -14,18 +14,18 @@ class GNTitleLabel: UILabel {
     }
     
     
-    convenience init(fontSize: CGFloat) {
+    convenience init(fontSize: CGFloat, textColour: UIColor = .black) {
         self.init(frame: .zero)
         let traits          = [UIFontDescriptor.TraitKey.weight: UIFont.Weight.bold]
         var descriptor      = UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.family: "Poppins"])
         descriptor          = descriptor.addingAttributes([UIFontDescriptor.AttributeName.traits: traits])
         font                = UIFont(descriptor: descriptor, size: fontSize)
+        textColor           = textColour
     }
     
     
     // MARK: - Methods
     private func configure() {
-        textColor                   = .black
         textAlignment               = .left
         adjustsFontSizeToFitWidth   = true
         minimumScaleFactor          = 0.9
