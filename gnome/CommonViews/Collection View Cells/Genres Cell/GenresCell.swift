c9cb8c3706a74d738c1f56d686a3839bcb3127db
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-02 08:53:04 +0530

    Genres Cell designed

--- gnome/CommonViews/Collection View Cells/Genres Cell/GenresCell.swift
+++ gnome/CommonViews/Collection View Cells/Genres Cell/GenresCell.swift
@@ -2,4 +2,30 @@ import UIKit
 
 class GenresCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    private let titleLabel  = GNTitleLabel(fontSize: 20, textColour: .white)
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Methods
+    private func configure() {
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
