68f847b51d53717dcedc64ba7876d4b22c11a11d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-04 21:21:06 +0530

    Tempory artists created

--- gnome/Model/Artist.swift
+++ gnome/Model/Artist.swift
@@ -3,7 +3,16 @@ import Foundation
 struct Artist {
     
     let name: String
-    let age: Int
-    let songs: [Song]
-    let albums: [Album]?
+    
+    static func fetchAllArtists() -> [Artist] {
+        
+        return [
+            Artist(name: "DIVINE"),
+            Artist(name: "DJ Snake"),
+            Artist(name: "RITVIZ"),
+            Artist(name: "Nucleya"),
+            Artist(name: "Ed Sheeran"),
+            Artist(name: "Kavya Trehan"),
+        ]
+    }
 }
