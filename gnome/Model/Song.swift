2ffc4a56de0ccb75ab6cc189cd15ac4637ad2ccb
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-05 11:52:09 +0530

    Model optimised

--- gnome/Model/Song.swift
+++ gnome/Model/Song.swift
@@ -1,4 +1,4 @@
-import UIKit
+import Foundation
 
 struct Song {
     
@@ -6,18 +6,17 @@ struct Song {
     let artist: String
     let thumbnailUrl: String
     let numberOfPlays: Int
-    let thumbnailImage: UIImage?
     
     
     static func fetchSongs() -> [Song] {
         
         return [
-            Song(title: "Bass Rani", artist: "Nucleya", thumbnailUrl: "", numberOfPlays: 500000, thumbnailImage: Temporary.song1),
-            Song(title: "Gully Boy", artist: "Various Artists", thumbnailUrl: "", numberOfPlays: 600000, thumbnailImage: Temporary.song2),
-            Song(title: "X", artist: "Ed Sheeran", thumbnailUrl: "", numberOfPlays: 800000, thumbnailImage: Temporary.song3),
-            Song(title: "Shape Of You", artist: "Ed Sheeran", thumbnailUrl: "", numberOfPlays: 1000000, thumbnailImage: Temporary.song4),
-            Song(title: "Kohinoor", artist: "DIVINE", thumbnailUrl: "", numberOfPlays: 100000, thumbnailImage: Temporary.song5),
-            Song(title: "Lights", artist: "Nuclea", thumbnailUrl: "", numberOfPlays: 300000, thumbnailImage: Temporary.song6),
+            Song(title: "Bass Rani", artist: "Nucleya", thumbnailUrl: Temporary.song1, numberOfPlays: 500000),
+            Song(title: "Gully Boy", artist: "Various Artists", thumbnailUrl: Temporary.song2, numberOfPlays: 600000),
+            Song(title: "X", artist: "Ed Sheeran", thumbnailUrl: Temporary.song3, numberOfPlays: 800000),
+            Song(title: "Shape Of You", artist: "Ed Sheeran", thumbnailUrl: Temporary.song4, numberOfPlays: 1000000),
+            Song(title: "Kohinoor", artist: "DIVINE", thumbnailUrl: Temporary.song5, numberOfPlays: 100000),
+            Song(title: "Lights", artist: "Nuclea", thumbnailUrl: Temporary.song6, numberOfPlays: 300000),
         ]
     }
     
@@ -25,12 +24,12 @@ struct Song {
     static func fetchRecommendedForYouSongs() -> [Song] {
         
         return [
-            Song(title: "Kohinoor", artist: "DIVINE", thumbnailUrl: "", numberOfPlays: 100000, thumbnailImage: Temporary.largeSong1),
-            Song(title: "Bass Rani", artist: "Nucleya", thumbnailUrl: "", numberOfPlays: 500000, thumbnailImage: Temporary.largeSong2),
-            Song(title: "X", artist: "Ed Sheeran", thumbnailUrl: "", numberOfPlays: 800000, thumbnailImage: Temporary.largeSong3),
-            Song(title: "Lights", artist: "Nuclea", thumbnailUrl: "", numberOfPlays: 300000, thumbnailImage: Temporary.largeSong1),
-            Song(title: "Gully Boy", artist: "Various Artists", thumbnailUrl: "", numberOfPlays: 600000, thumbnailImage: Temporary.largeSong4),
-            Song(title: "Shape Of You", artist: "Ed Sheeran", thumbnailUrl: "", numberOfPlays: 1000000, thumbnailImage: Temporary.largeSong2),
+            Song(title: "Kohinoor", artist: "DIVINE", thumbnailUrl: Temporary.largeSong1, numberOfPlays: 100000),
+            Song(title: "Bass Rani", artist: "Nucleya", thumbnailUrl: Temporary.largeSong2, numberOfPlays: 500000),
+            Song(title: "X", artist: "Ed Sheeran", thumbnailUrl: Temporary.largeSong3, numberOfPlays: 800000),
+            Song(title: "Lights", artist: "Nuclea", thumbnailUrl: Temporary.largeSong4, numberOfPlays: 300000),
+            Song(title: "Gully Boy", artist: "Various Artists", thumbnailUrl: Temporary.largeSong1, numberOfPlays: 600000),
+            Song(title: "Shape Of You", artist: "Ed Sheeran", thumbnailUrl: Temporary.largeSong3, numberOfPlays: 1000000),
         ]
     }
 }
