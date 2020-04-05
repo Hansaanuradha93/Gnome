3aef5a442ff0b91218a5cacd900aada5674d2df6
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-05 13:00:02 +0530

    fetchArtists() added to Artist model

--- gnome/Model/Artist.swift
+++ gnome/Model/Artist.swift
@@ -3,17 +3,18 @@ import Foundation
 struct Artist {
     
     let name: String
+    let thumbnailUrl: String
     
     
     static func fetchAllArtists() -> [Artist] {
         
         return [
-            Artist(name: "DIVINE"),
-            Artist(name: "DJ Snake"),
-            Artist(name: "RITVIZ"),
-            Artist(name: "Nucleya"),
-            Artist(name: "Ed Sheeran"),
-            Artist(name: "Kavya Trehan"),
+            Artist(name: "DIVINE", thumbnailUrl: Temporary.artist1),
+            Artist(name: "DJ Snake", thumbnailUrl: Temporary.artist2),
+            Artist(name: "RITVIZ", thumbnailUrl: Temporary.artist3),
+            Artist(name: "Nucleya", thumbnailUrl: Temporary.artist4),
+            Artist(name: "Ed Sheeran", thumbnailUrl: Temporary.artist5),
+            Artist(name: "Kavya Trehan", thumbnailUrl: Temporary.artist2),
         ]
     }
 }
