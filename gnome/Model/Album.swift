71cd6a76d0f28bdf4e23b5cec74f2c2aca98391e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-05 12:28:22 +0530

    fetchAlbums() added to Album model

--- gnome/Model/Album.swift
+++ gnome/Model/Album.swift
@@ -10,11 +10,11 @@ struct Album {
     static func fetchAlbums() -> [Album] {
         
         return [
-            Album(title: "Raja Baja", artist: "Kavya Trehan", thumbnailUrl: ""),
-            Album(title: "The GaryVee Audio Experience", artist: "Gerry Flinch", thumbnailUrl: ""),
-            Album(title: "Wireframe", artist: "MKHD", thumbnailUrl: ""),
-            Album(title: "Design Better Podcast", artist: "Sean Allen", thumbnailUrl: ""),
-            Album(title: "Kohinoor", artist: "Divine", thumbnailUrl: "")
+            Album(title: "Raja Baja", artist: "Kavya Trehan", thumbnailUrl: Temporary.album1),
+            Album(title: "The GaryVee Audio Experience", artist: "Gerry Flinch", thumbnailUrl: Temporary.album2),
+            Album(title: "Wireframe", artist: "MKHD", thumbnailUrl: Temporary.album3),
+            Album(title: "Design Better Podcast", artist: "Sean Allen", thumbnailUrl: Temporary.album4),
+            Album(title: "Kohinoor", artist: "Divine", thumbnailUrl: Temporary.album1)
         ]
     }
 }
