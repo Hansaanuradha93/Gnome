516588dbfdac678967f64844a096850f81f474b2
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-06 08:33:43 +0530

    Collection View configured in TopTrendingVC

--- gnome/CommonViews/View Controllers/Top Trending/TopTrendingVC.swift
+++ gnome/CommonViews/View Controllers/Top Trending/TopTrendingVC.swift
@@ -2,7 +2,91 @@ import UIKit
 
 class TopTrendingVC: UIViewController {
 
+    private let titleLabel      = GNTitleLabel(fontSize: 22)
+    private var collectionView  : UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTitleLabel()
+        configureCollectionView()
+    }
+}
+
+
+// MARK: - Methods
+extension TopTrendingVC {
+    
+    private func configureTitleLabel() {
+        
+        view.addSubview(titleLabel)
+        titleLabel.text = Titles.topTrending
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 32)
+        ])
+    }
+    
+    
+    private func configureCollectionView() {
+        
+        collectionView                  = UICollectionView(frame: .zero, collectionViewLayout: createFlowLayout())
+        collectionView.backgroundColor  = .systemBackground
+        collectionView.dataSource       = self
+        collectionView.delegate         = self
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(SongCell.self, forCellWithReuseIdentifier: SongCell.reuseID)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 14),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    
+    private func createFlowLayout() -> UICollectionViewFlowLayout {
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection      = .vertical
+        flowLayout.sectionInset         = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        flowLayout.minimumLineSpacing   = 20.7
+        return flowLayout
+    }
+}
+
+
+// MARK: - Collection View Data Source
+extension TopTrendingVC: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int { return 1 }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { return 10 }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SongCell.reuseID, for: indexPath) as! SongCell
+        return cell
+    }
+}
+
+
+// MARK: - FlowLayout Delegate
+extension TopTrendingVC: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let flowLayout          = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let width               = UIScreen.main.bounds.size.width - (flowLayout.sectionInset.left + flowLayout.sectionInset.right)
+        return CGSize(width: width, height: 115)
+
     }
 }
