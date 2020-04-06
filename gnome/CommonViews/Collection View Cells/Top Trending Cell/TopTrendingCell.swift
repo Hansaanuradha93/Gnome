f2a5b703d04482003ce11f4e89d3b5009649bc26
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-06 08:51:08 +0530

    Thumbnail image view added to TopTrendingCell

--- gnome/CommonViews/Collection View Cells/Top Trending Cell/TopTrendingCell.swift
+++ gnome/CommonViews/Collection View Cells/Top Trending Cell/TopTrendingCell.swift
@@ -1,13 +1,31 @@
-//
-//  TopTrendingCell.swift
-//  gnome
-//
-//  Created by Hansa Anuradha on 4/6/20.
-//  Copyright © 2020 Hansa Anuradha. All rights reserved.
-//
-
 import UIKit
 
 class TopTrendingCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    static let reuseID              = "TopTrendingCell"
+    private let thumbnailImageView  = GNThumbnaiImageView(frame: .zero)
+    
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureThumbnailImageView()
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    
+    // MARK: - Methods
+    private func configureThumbnailImageView() {
+        addSubview(thumbnailImageView)
+        
+        NSLayoutConstraint.activate([
+            thumbnailImageView.topAnchor.constraint(equalTo: topAnchor),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            thumbnailImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            thumbnailImageView.widthAnchor.constraint(equalTo: thumbnailImageView.heightAnchor)
+        ])
+    }
 }
