50971191dd383394952ee64afd6dc7deb82228d5
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-06 08:50:23 +0530

    TopTrendingCell added

---
+++ gnome/CommonViews/Collection View Cells/Top Trending Cell/TopTrendingCell.swift
@@ -0,0 +1,13 @@
+//
+//  TopTrendingCell.swift
+//  gnome
+//
+//  Created by Hansa Anuradha on 4/6/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class TopTrendingCell: UICollectionViewCell {
+    
+}
