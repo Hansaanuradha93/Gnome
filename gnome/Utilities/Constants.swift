c552f6be43484581ac888c3b87f871e891c58882
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-07 08:43:45 +0530

    Constants added

--- gnome/Utilities/Constants.swift
+++ gnome/Utilities/Constants.swift
@@ -9,6 +9,7 @@ enum Asserts {
     static let settings     = UIImage(named: "settings")
     static let placeholder  = UIImage(named: "library_music")
     static let smallPlay    = UIImage(named: "small_play")
+    static let more         = UIImage(named: "more_vert")
 }
 
 
