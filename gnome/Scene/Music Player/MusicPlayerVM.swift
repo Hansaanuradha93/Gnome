4fcb0449ae3ced6cb366aa8dcc65c6d237f9d688
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-08 09:02:49 +0530

    MusicPlayerVM customised

--- gnome/Scene/Music Player/MusicPlayerVM.swift
+++ gnome/Scene/Music Player/MusicPlayerVM.swift
@@ -1,9 +1,3 @@
-//
-//  MusicPlayerVM.swift
-//  gnome
-//
-//  Created by Hansa Anuradha on 4/8/20.
-//  Copyright © 2020 Hansa Anuradha. All rights reserved.
-//
-
 import Foundation
+
+class MusicPlayerVM {}
