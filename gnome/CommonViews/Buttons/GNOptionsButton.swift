5889b8fec4e5f16c7b94fa4257bb6303c70af83e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-09 08:55:12 +0530

    GNOptionsButton added

---
+++ gnome/CommonViews/Buttons/GNOptionsButton.swift
@@ -0,0 +1,21 @@
+//
+//  GNOptionsButton.swift
+//  gnome
+//
+//  Created by Hansa Anuradha on 4/9/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class GNOptionsButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
