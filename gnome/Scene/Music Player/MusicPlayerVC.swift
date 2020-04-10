682020d3fbadbb0bc76066d63c17884e7feea921
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-10 09:08:50 +0530

    NowPlayingLebel added

--- gnome/Scene/Music Player/MusicPlayerVC.swift
+++ gnome/Scene/Music Player/MusicPlayerVC.swift
@@ -5,11 +5,13 @@ class MusicPlayerVC: UIViewController {
     private let viewModel = MusicPlayerVM()
     private let collapseButton = GNAssertButton(assert: Asserts.collapse)
     private let optionButton = GNAssertButton(assert: Asserts.option)
+    private let nowPlayingLabel = GNSecondaryTitleLabel(fontSize: 13, alignment: .center)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureButtons()
+        configureNowPlayingLabel()
     }
     
     
@@ -44,10 +46,25 @@ extension MusicPlayerVC {
             collapseButton.widthAnchor.constraint(equalToConstant: 16.98),
             collapseButton.heightAnchor.constraint(equalToConstant: 10.48),
             
-            optionButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 73),
+            optionButton.topAnchor.constraint(equalTo: collapseButton.topAnchor),
             optionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -29),
             optionButton.widthAnchor.constraint(equalToConstant: 16),
             optionButton.heightAnchor.constraint(equalToConstant: 14)
+        ])
+    }
+    
+    
+    private func configureNowPlayingLabel() {
+        
+        view.addSubview(nowPlayingLabel)
+        nowPlayingLabel.alpha = 0.6
+        nowPlayingLabel.text = "NOW PLAYING FROM"
+        
+        NSLayoutConstraint.activate([
+            nowPlayingLabel.topAnchor.constraint(equalTo: collapseButton.topAnchor),
+            nowPlayingLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nowPlayingLabel.widthAnchor.constraint(equalToConstant: 140),
+            nowPlayingLabel.heightAnchor.constraint(equalToConstant: 13)
         ])
     }
 }
