adbf65dc8d95591bb2cdd8e81dc6b18df49ea5c8
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-11 11:11:59 +0530

    Basic ui configurations of SongContainerVC is done

--- gnome/CommonViews/View Controllers/Song Container/SongContainerVC.swift
+++ gnome/CommonViews/View Controllers/Song Container/SongContainerVC.swift
@@ -22,6 +22,23 @@ class SongContainerVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureViewController()
+    }
+}
+
+
+// MARK: - Methods
+extension SongContainerVC {
+    
+    private func configureViewController() {
+        
+        view.backgroundColor = .systemBackground
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
 }
