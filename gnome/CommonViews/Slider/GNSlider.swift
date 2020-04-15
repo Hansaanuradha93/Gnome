60fcc8700dd86ab765120cc9260069256063aabb
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-15 08:59:25 +0530

    GNSlider added

---
+++ gnome/CommonViews/Slider/GNSlider.swift
@@ -0,0 +1,21 @@
+//
+//  GNSlider.swift
+//  gnome
+//
+//  Created by Hansa Anuradha on 4/15/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class GNSlider: UISlider {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
