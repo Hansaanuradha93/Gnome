8d55533ee23183694d1007fba53a1d963f1c5f2c
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-23 09:16:19 +0530

    Expan screen basic configurations

--- gnome/Scene/Explore/Expan/ExpanVC.swift
+++ gnome/Scene/Explore/Expan/ExpanVC.swift
@@ -1,30 +1,29 @@
-//
-//  ExpanVC.swift
-//  gnome
-//
-//  Created by Hansa Anuradha on 4/23/20.
-//  Copyright © 2020 Hansa Anuradha. All rights reserved.
-//
-
 import UIKit
 
 class ExpanVC: UIViewController {
 
+    private let viewModel = ExploreVM()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureViewController()
     }
-    */
+}
 
+
+// MARK: - Methods
+extension ExpanVC {
+    
+    private func configureViewController() {
+        
+        view.backgroundColor = .systemBackground
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
 }
