274e3ad9b71bf5a3574e98d06c84e193489e5186
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-24 08:27:43 +0530

    ExpanArtistCell basic configurations done

--- gnome/CommonViews/Collection View Cells/Expan Artist/ExpanArtistCell.swift
+++ gnome/CommonViews/Collection View Cells/Expan Artist/ExpanArtistCell.swift
@@ -2,4 +2,15 @@ import UIKit
 
 class ExpanArtistCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    static let reuseID      = "ExpanArtistCell"
+    
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 }
