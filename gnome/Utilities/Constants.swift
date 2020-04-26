64b13ff3da44d723ed55ccd3078744d840074289
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-26 20:42:47 +0530

    Constants added

--- gnome/Utilities/Constants.swift
+++ gnome/Utilities/Constants.swift
@@ -23,6 +23,7 @@ enum Asserts {
     static let shuffle      = UIImage(named: "shuffle")!
     static let queueMusic   = UIImage(named: "queue_music")!
     static let sliderThumb  = UIImage(named: "slider_thumb")!
+    static let back         = UIImage(named: "back")!
 }
 
 
