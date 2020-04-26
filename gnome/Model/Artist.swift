31893dc84ab38e24f14393eec3789a6e29e311ea
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-26 20:07:04 +0530

    Artist model changed

--- gnome/Model/Artist.swift
+++ gnome/Model/Artist.swift
@@ -4,17 +4,17 @@ struct Artist {
     
     let name: String
     let thumbnailUrl: String
-    
+    let largeThumbnailUrl: String
     
     static func fetchAllArtists() -> [Artist] {
         
         return [
-            Artist(name: "DIVINE", thumbnailUrl: Temporary.artist1),
-            Artist(name: "DJ Snake", thumbnailUrl: Temporary.artist2),
-            Artist(name: "RITVIZ", thumbnailUrl: Temporary.artist3),
-            Artist(name: "Nucleya", thumbnailUrl: Temporary.artist4),
-            Artist(name: "Ed Sheeran", thumbnailUrl: Temporary.artist5),
-            Artist(name: "Kavya Trehan", thumbnailUrl: Temporary.artist2),
+            Artist(name: "DIVINE", thumbnailUrl: Temporary.artist1, largeThumbnailUrl: Temporary.artistLarge1),
+            Artist(name: "DJ Snake", thumbnailUrl: Temporary.artist2, largeThumbnailUrl: Temporary.artistLarge1),
+            Artist(name: "RITVIZ", thumbnailUrl: Temporary.artist3, largeThumbnailUrl: Temporary.artistLarge1),
+            Artist(name: "Nucleya", thumbnailUrl: Temporary.artist4, largeThumbnailUrl: Temporary.artistLarge1),
+            Artist(name: "Ed Sheeran", thumbnailUrl: Temporary.artist5, largeThumbnailUrl: Temporary.artistLarge1),
+            Artist(name: "Kavya Trehan", thumbnailUrl: Temporary.artist2, largeThumbnailUrl: Temporary.artistLarge1),
         ]
     }
 }
