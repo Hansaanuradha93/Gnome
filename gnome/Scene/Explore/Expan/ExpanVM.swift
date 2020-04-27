edf781d6267aff4df85b35d045cd278b6d445177
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-27 09:39:59 +0530

    Sections added to ExpanVM

--- gnome/Scene/Explore/Expan/ExpanVM.swift
+++ gnome/Scene/Explore/Expan/ExpanVM.swift
@@ -1,3 +1,33 @@
 import Foundation
 
-class ExpanVM {}
+class ExpanVM {
+    
+    enum SectionType: Int {
+        case thumbnail             = 0
+        case popularSongs          = 1
+        case albums                = 2
+    }
+    
+    
+    struct Section {
+        
+        var sectionType: SectionType
+        var index: Int
+    }
+    
+    
+    lazy var sections: [Section]   = {
+        
+        var data = [Section]()
+        
+        let thumbnailSection       = Section(sectionType: .thumbnail, index: 0)
+        let popularSongsSection    = Section(sectionType: .popularSongs, index: 1)
+        let albumsSection          = Section(sectionType: .albums, index: 2)
+
+        data.append(thumbnailSection)
+        data.append(popularSongsSection)
+        data.append(albumsSection)
+        
+        return data
+    }()
+}
