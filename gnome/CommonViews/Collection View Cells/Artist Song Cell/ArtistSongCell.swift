3d6a73c3b96f4fab363c8721d42deee1d110f771
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-27 09:26:24 +0530

    ArtistSongCell added

---
+++ gnome/CommonViews/Collection View Cells/Artist Song Cell/ArtistSongCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArtistSongCell.swift
+//  gnome
+//
+//  Created by Hansa Anuradha on 4/27/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ArtistSongCell: UICollectionViewCell {
+    
+}
