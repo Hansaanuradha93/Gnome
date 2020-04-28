a1248b2c069e87f03f9628af38892c8e5542b5d0
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-28 23:09:09 +0530

    ThumbnaimImageView added to the PopularSongCell

--- gnome/CommonViews/Collection View Cells/Popular Song Cell/PopularSongCell.swift
+++ gnome/CommonViews/Collection View Cells/Popular Song Cell/PopularSongCell.swift
@@ -4,13 +4,34 @@ class PopularSongCell: UICollectionViewCell {
     
     // MARK: - Properties
     static let reuseID              = "PopularSongCell"
+    private let thumbnailImageView  = GNThumbnaiImageView(frame: .zero)
     
     
     // MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        configureThumbnailImageView()
     }
     
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+
+// MARK: - Methods
+extension PopularSongCell {
+    
+    private func configureThumbnailImageView() {
+        
+        thumbnailImageView.image = UIImage(named: Temporary.song1)
+        addSubview(thumbnailImageView)
+        
+        NSLayoutConstraint.activate([
+            thumbnailImageView.topAnchor.constraint(equalTo: topAnchor),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            thumbnailImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            thumbnailImageView.widthAnchor.constraint(equalTo: heightAnchor)
+        ])
+    }
 }
