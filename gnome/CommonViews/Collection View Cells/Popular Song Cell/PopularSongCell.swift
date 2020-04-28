b4b04b271ceb21fb5cad8b00edadfa2755382201
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-28 22:56:30 +0530

    PopularSongCell added

---
+++ gnome/CommonViews/Collection View Cells/Popular Song Cell/PopularSongCell.swift
@@ -0,0 +1,13 @@
+//
+//  PopularSongCell.swift
+//  gnome
+//
+//  Created by Hansa Anuradha on 4/28/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class PopularSongCell: UICollectionViewCell {
+    
+}
