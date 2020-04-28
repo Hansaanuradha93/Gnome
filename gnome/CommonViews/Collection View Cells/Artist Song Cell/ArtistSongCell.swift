b67f28140f2f324805008f09f7e74562e39f3322
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-28 22:50:14 +0530

    Collection view added to ArtistSongCell

--- gnome/CommonViews/Collection View Cells/Artist Song Cell/ArtistSongCell.swift
+++ gnome/CommonViews/Collection View Cells/Artist Song Cell/ArtistSongCell.swift
@@ -5,12 +5,14 @@ class ArtistSongCell: UICollectionViewCell {
     // MARK: - Properties
     static let reuseID              = "ArtistSongCell"
     private let titleLabel          = GNTitleLabel(fontSize: 20)
+    private var collectionView      : UICollectionView!
     
     // MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         configureTitleLabel()
+        configureCollectionView()
     }
     
     
@@ -30,5 +32,47 @@ extension ArtistSongCell {
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor)
         ])
+    }
+    
+    
+    private func configureCollectionView() {
+        
+        collectionView                  = UICollectionView(frame: .zero, collectionViewLayout: createFlowLayout())
+        collectionView.backgroundColor  = .systemBackground
+        collectionView.dataSource       = self
+//        collectionView.delegate         = self
+        
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(ArtistSongCell.self, forCellWithReuseIdentifier: ArtistSongCell.reuseID)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 22),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    
+    private func createFlowLayout() -> UICollectionViewFlowLayout {
+        
+        let flowLayout              = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection  = .vertical
+        flowLayout.sectionInset     = UIEdgeInsets(top: 0, left: 16, bottom: 28, right: 16)
+        return flowLayout
+    }
+}
+
+
+// MARK: - Collection View Data Source
+extension ArtistSongCell: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { return 3}
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ArtistSongCell.reuseID, for: indexPath) as! ArtistSongCell
+        return cell
     }
 }
