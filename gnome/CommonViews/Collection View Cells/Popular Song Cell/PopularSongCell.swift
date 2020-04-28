9b768c1fdc8418611b8d333ad5a7720135a1dc2f
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-04-28 22:58:21 +0530

    PopularSongCell basic configurations

--- gnome/CommonViews/Collection View Cells/Popular Song Cell/PopularSongCell.swift
+++ gnome/CommonViews/Collection View Cells/Popular Song Cell/PopularSongCell.swift
@@ -1,13 +1,16 @@
-//
-//  PopularSongCell.swift
-//  gnome
-//
-//  Created by Hansa Anuradha on 4/28/20.
-//  Copyright © 2020 Hansa Anuradha. All rights reserved.
-//
-
 import UIKit
 
 class PopularSongCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    static let reuseID              = "PopularSongCell"
+    
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 }
