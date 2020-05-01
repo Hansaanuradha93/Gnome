97b02211758c947f4666ba4129b9562c5fd04653
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-05-01 22:49:57 +0530

    TitleLabel added to ExpanRowCell

--- gnome/CommonViews/Collection View Cells/Expan Row Cell/ExpanRowCell.swift
+++ gnome/CommonViews/Collection View Cells/Expan Row Cell/ExpanRowCell.swift
@@ -4,13 +4,36 @@ class ExpanRowCell: UICollectionViewCell {
     
     // MARK: - Properties
     static let reuseID                  = "ExpanRowCell"
+    private let titleLabel              = GNTitleLabel(fontSize: 22)
+    private var collectionView          : UICollectionView!
     
     
     // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureTitleLabel()
     }
     
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 }
+
+
+// MARK: - Methods
+extension ExpanRowCell {
+    
+    
+    private func configureTitleLabel() {
+        
+        titleLabel.text = "Albums"
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 22),
+        ])
+    }
+}
+
