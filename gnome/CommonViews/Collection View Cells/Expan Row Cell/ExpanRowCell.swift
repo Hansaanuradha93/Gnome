aaf400a9a8e8654edca39f56ed9af9fa7da1d36e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-05-01 23:08:22 +0530

    CollectionView added to ExpanRowCell

--- gnome/CommonViews/Collection View Cells/Expan Row Cell/ExpanRowCell.swift
+++ gnome/CommonViews/Collection View Cells/Expan Row Cell/ExpanRowCell.swift
@@ -4,7 +4,7 @@ class ExpanRowCell: UICollectionViewCell {
     
     // MARK: - Properties
     static let reuseID                  = "ExpanRowCell"
-    private let titleLabel              = GNTitleLabel(fontSize: 22)
+    private let titleLabel              = GNTitleLabel(fontSize: 20)
     private var collectionView          : UICollectionView!
     
     
@@ -12,6 +12,7 @@ class ExpanRowCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureTitleLabel()
+        configureCollectionView()
         backgroundColor = .red
     }
     
@@ -35,5 +36,34 @@ extension ExpanRowCell {
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
             titleLabel.heightAnchor.constraint(equalToConstant: 22),
         ])
+    }
+    
+    
+    private func configureCollectionView() {
+        
+        collectionView                                  = UICollectionView(frame: .zero, collectionViewLayout: createFlowLayout())
+        collectionView.backgroundColor                  = .systemBackground
+        collectionView.showsHorizontalScrollIndicator   = false
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 22),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    
+    private func createFlowLayout() -> UICollectionViewFlowLayout {
+        
+        let flowLayout                      = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection          = .horizontal
+        flowLayout.sectionInset             = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        flowLayout.minimumInteritemSpacing  = 15
+        return flowLayout
     }
 }
