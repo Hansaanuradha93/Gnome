74d2a04b8b2b35105334e99d07ff60652fc5f0f4
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-05-01 22:46:45 +0530

    ExpanRowCell added

---
+++ gnome/CommonViews/Collection View Cells/Expan Row Cell/ExpanRowCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExpanRowCell.swift
+//  gnome
+//
+//  Created by Hansa Anuradha on 5/1/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ExpanRowCell: UICollectionViewCell {
+    
+}
