cb04f498f8dd96fc2adadf6004368fc1d371b7bc
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-13 22:41:40 +0530

    PlayerContainerVC code refactored

--- gnome/CommonViews/View Controllers/Player Container/PlayerContainerVC.swift
+++ gnome/CommonViews/View Controllers/Player Container/PlayerContainerVC.swift
@@ -6,20 +6,22 @@ protocol PlayerContainerDelegate: class { func buttonPressed(index: Int)}
 
 class PlayerContainerVC: UIViewController {
 
-    private let viewModel           = PlayerContainerVM()
-    private let stackView           = UIStackView()
-    private let slider              = GNSlider(frame: .zero)
-    private let sliderMinimumLabel  = GNSecondaryTitleLabel(fontSize: 11, fontColor: UIColor.appColor(.Pretty_Pink))
-    private let sliderMaximumLabel  = GNSecondaryTitleLabel(fontSize: 11, fontColor: UIColor.appColor(.Pretty_Pink))
-    private let playButton          = GNAssertButton(assert: Asserts.play)
-    private let backwardButton      = GNAssertButton(assert: Asserts.rewind)
-    private let rewind30Button      = GNAssertButton(assert: Asserts.rewind30)
-    private let forwadButton        = GNAssertButton(assert: Asserts.forward)
-    private let forward30Button     = GNAssertButton(assert: Asserts.forward30)
-    private let favouriteButton     = GNAssertButton(assert: Asserts.favorite, contentMode: .center)
-    private let shuffleButton       = GNAssertButton(assert: Asserts.shuffle, contentMode: .center)
-    private let repeatButton        = GNAssertButton(assert: Asserts.repeatAgain, contentMode: .center)
-    private let queueMusicButton    = GNAssertButton(assert: Asserts.queueMusic, contentMode: .center)
+    // MARK: Properties
+    private let viewModel = PlayerContainerVM()
+    
+    private let stackView = UIStackView()
+    private let slider = GNSlider(frame: .zero)
+    private let sliderMinimumLabel = GNSecondaryTitleLabel(fontSize: 11, fontColor: UIColor.appColor(.Pretty_Pink))
+    private let sliderMaximumLabel = GNSecondaryTitleLabel(fontSize: 11, fontColor: UIColor.appColor(.Pretty_Pink))
+    private let playButton = GNAssertButton(assert: Asserts.play)
+    private let backwardButton = GNAssertButton(assert: Asserts.rewind)
+    private let rewind30Button = GNAssertButton(assert: Asserts.rewind30)
+    private let forwadButton = GNAssertButton(assert: Asserts.forward)
+    private let forward30Button = GNAssertButton(assert: Asserts.forward30)
+    private let favouriteButton = GNAssertButton(assert: Asserts.favorite, contentMode: .center)
+    private let shuffleButton = GNAssertButton(assert: Asserts.shuffle, contentMode: .center)
+    private let repeatButton = GNAssertButton(assert: Asserts.repeatAgain, contentMode: .center)
+    private let queueMusicButton = GNAssertButton(assert: Asserts.queueMusic, contentMode: .center)
 
     private var player: AVAudioPlayer!
     private var songs: [Song]!
@@ -27,23 +29,23 @@ class PlayerContainerVC: UIViewController {
     var playerContainerDelegate: PlayerContainerDelegate?
     
     
+    // MARK: Initializers
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) { super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil) }
     
     
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    required init?(coder: NSCoder) { fatalError() }
     
     
     convenience init(songs: [Song], index: Int) {
         self.init()
-        
         self.songs = songs
         self.index = index
     }
     
     
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configureSlider()
         configureSliderLabels()
         configurePlayButton()
@@ -62,7 +64,6 @@ class PlayerContainerVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         configureViewController()
     }
 }
@@ -72,34 +73,29 @@ class PlayerContainerVC: UIViewController {
 extension PlayerContainerVC {
     
     @objc func sliderChanged() {
-        
-        sliderMinimumLabel.text     = Double(slider.value).getTimeFormat()
+        sliderMinimumLabel.text = Double(slider.value).getTimeFormat()
         player.stop()
-        player.currentTime          = TimeInterval(slider.value)
+        player.currentTime = TimeInterval(slider.value)
         player.play()
     }
     
     
     @objc func updateSlider() {
-        
         if player.isPlaying {
             sliderMinimumLabel.text = player.currentTime.getTimeFormat()
-            slider.value            = Float(Int(player.currentTime))
+            slider.value = Float(Int(player.currentTime))
         }
     }
     
     
     private func prepareSongSesstion(song: Song) {
-        
-        guard
-            let urlString   = Bundle.main.path(forResource: song.file.name, ofType: song.file.type),
-            let url         = URL(string: urlString) else  { return }
+        guard let urlString = Bundle.main.path(forResource: song.file.name, ofType: song.file.type), let url = URL(string: urlString) else  { return }
         
         do {
-            player          = try AVAudioPlayer(contentsOf: url)
+            player = try AVAudioPlayer(contentsOf: url)
             player.prepareToPlay()
             
-            let session     = AVAudioSession.sharedInstance()
+            let session = AVAudioSession.sharedInstance()
             
             do { try session.setCategory(.playback) }
             catch let sessionError { print("Session Error: \(sessionError)") }
@@ -109,17 +105,15 @@ extension PlayerContainerVC {
 
     
     private func configureSongPlayer() {
-        
         player.play()
         
-        slider.maximumValue     = Float(Int(player.duration))
+        slider.maximumValue = Float(Int(player.duration))
         sliderMinimumLabel.text = player.currentTime.getTimeFormat()
         sliderMaximumLabel.text = player.duration.getTimeFormat()
         
         Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateSlider), userInfo: nil, repeats: true)
         
-        playButton.action       = { () in
-            
+        playButton.action = { () in
             if self.player.isPlaying {
                 self.playButton.placeholderImageView.image = Asserts.pause
                 self.player.pause()
@@ -132,13 +126,11 @@ extension PlayerContainerVC {
     
     
     private func configureForwardButtonAction() {
-        
-        forwadButton.action     = { () in
-            
-            if self.index       == self.songs.count - 1 {
-                self.index      = 0
+        forwadButton.action = { () in
+            if self.index == self.songs.count - 1 {
+                self.index = 0
             } else {
-                self.index      = self.index + 1
+                self.index = self.index + 1
             }
             self.playerContainerDelegate?.buttonPressed(index: self.index )
             self.playNextSong()
@@ -147,10 +139,8 @@ extension PlayerContainerVC {
     
     
     private func configureBackwardButtonAction() {
-        
-        backwardButton.action   = { () in
-            
-            if self.index      != 0 { self.index = self.index - 1 }
+        backwardButton.action = { () in
+            if self.index != 0 { self.index = self.index - 1 }
             self.playerContainerDelegate?.buttonPressed(index: self.index)
             self.playNextSong()
         }
@@ -158,55 +148,52 @@ extension PlayerContainerVC {
     
     
     private func playNextSong() {
-        
         player.stop()
         prepareSongSesstion(song: self.songs[self.index])
         player.play()
         sliderMaximumLabel.text = player.duration.getTimeFormat()
-        slider.maximumValue     = Float(Int(player.duration))
+        slider.maximumValue = Float(Int(player.duration))
     }
     
     
     private func configureRewind30Button() {
-        
-        rewind30Button.action               = { () in
+        rewind30Button.action = { () in
             
             self.player.stop()
-            var time                        = self.player.currentTime
-            time                            = time - 30
+            var time = self.player.currentTime
+            time = time - 30
             
             if time < 0 {
-                self.player.currentTime     = 0
+                self.player.currentTime = 0
             } else {
-                self.player.currentTime     = time
+                self.player.currentTime = time
             }
             
             self.player.play()
-            self.slider.value               = Float(Int(self.player.currentTime))
-            self.sliderMinimumLabel.text    = self.player.currentTime.getTimeFormat()
+            self.slider.value = Float(Int(self.player.currentTime))
+            self.sliderMinimumLabel.text = self.player.currentTime.getTimeFormat()
         }
     }
     
     
     private func configureForward30Button() {
-        
         forward30Button.action              = { () in
             
             self.player.stop()
-            var time                        = self.player.currentTime
-            time                            = time + 30
+            var time = self.player.currentTime
+            time = time + 30
             
             if time > self.player.duration {
-                self.player.currentTime     = self.player.duration
-                self.slider.value           = Float(self.player.duration)
+                self.player.currentTime = self.player.duration
+                self.slider.value = Float(self.player.duration)
                 self.player.stop()
             } else {
-                self.player.currentTime     = time
+                self.player.currentTime = time
             }
             
             self.player.play()
-            self.slider.value               = Float(Int(self.player.currentTime))
-            self.sliderMinimumLabel.text    = self.player.currentTime.getTimeFormat()
+            self.slider.value = Float(Int(self.player.currentTime))
+            self.sliderMinimumLabel.text = self.player.currentTime.getTimeFormat()
         }
     }
 }
@@ -216,22 +203,19 @@ extension PlayerContainerVC {
 extension PlayerContainerVC {
 
     private func configureViewController() {
-        
         view.backgroundColor = .systemBackground
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     
     private func configureSlider() {
-        
         view.addSubview(slider)
-        
         slider.translatesAutoresizingMaskIntoConstraints = false
         
-        slider.minimumValue         = 0
-        slider.isContinuous         = true
-        slider.layer.cornerRadius   = 30
-        slider.tintColor            = UIColor.appColor(.Pretty_Pink)
+        slider.minimumValue = 0
+        slider.isContinuous = true
+        slider.layer.cornerRadius = 30
+        slider.tintColor = UIColor.appColor(.Pretty_Pink)
         
         slider.setThumbImage(Asserts.sliderThumb, for: .normal)
         slider.setThumbImage(Asserts.sliderThumb, for: .highlighted)
@@ -247,14 +231,12 @@ extension PlayerContainerVC {
     
     
     private func configureSliderLabels() {
-        
         view.addSubview(sliderMinimumLabel)
         view.addSubview(sliderMaximumLabel)
         
         NSLayoutConstraint.activate([
             sliderMinimumLabel.topAnchor.constraint(equalTo: slider.bottomAnchor, constant: 8),
             sliderMinimumLabel.leadingAnchor.constraint(equalTo: slider.leadingAnchor),
-            
             sliderMaximumLabel.topAnchor.constraint(equalTo: sliderMinimumLabel.topAnchor),
             sliderMaximumLabel.trailingAnchor.constraint(equalTo: slider.trailingAnchor)
         ])
@@ -262,7 +244,6 @@ extension PlayerContainerVC {
     
     
     private func configurePlayButton() {
-        
         let dimensions: CGFloat = 70
         view.addSubview(playButton)
                 
@@ -276,9 +257,8 @@ extension PlayerContainerVC {
     
     
     private func configureRewindButtons() {
-        
-        let dimension: CGFloat      = 36
-        let dimension30: CGFloat    = 24
+        let dimension: CGFloat = 36
+        let dimension30: CGFloat = 24
         
         view.addSubview(backwardButton)
         view.addSubview(rewind30Button)
@@ -310,8 +290,7 @@ extension PlayerContainerVC {
     
     
     private func configureStackView() {
-        
-        stackView.axis          = .horizontal
+        stackView.axis = .horizontal
         stackView.distribution  = .fillEqually
         
         stackView.addArrangedSubview(favouriteButton)
@@ -333,12 +312,11 @@ extension PlayerContainerVC {
     
     
     private func configureOtherButtons() {
+        let value: CGFloat = 0.5
         
-        let value: CGFloat      = 0.5
-        
-        favouriteButton.alpha   = value
-        shuffleButton.alpha     = value
-        repeatButton.alpha      = value
-        queueMusicButton.alpha  = value
+        favouriteButton.alpha = value
+        shuffleButton.alpha = value
+        repeatButton.alpha = value
+        queueMusicButton.alpha = value
     }
 }
