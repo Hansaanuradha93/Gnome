444478fbbb99f227cc9bb48f1b98afae72ab51ae
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-13 22:19:42 +0530

    Image Views refactored

--- gnome/CommonViews/Image Views/GNThumbnaiImageView.swift
+++ gnome/CommonViews/Image Views/GNThumbnaiImageView.swift
@@ -2,35 +2,35 @@ import UIKit
 
 class GNThumbnaiImageView: UIImageView {
     
-    // MARK: - Properties
+    // MARK: Properties
     private let placeholderImage = CommonImages.placeholder
     
     
-    // MARK: - Initializers
+    // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         configure()
     }
     
     
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
+    required init?(coder: NSCoder) { fatalError() }
     
     
     convenience init(radius: CGFloat) {
         self.init(frame: .zero)
-        
         layer.cornerRadius = radius
     }
-    
+}
+
+
+// MARK: - Methods
+extension GNThumbnaiImageView {
     
     private func configure() {
-        
-        layer.cornerRadius  = 15
-        clipsToBounds       = true
-        image               = placeholderImage
-        contentMode         = .scaleAspectFit
+        layer.cornerRadius = 15
+        clipsToBounds = true
+        image = placeholderImage
+        contentMode = .scaleAspectFit
         translatesAutoresizingMaskIntoConstraints = false
     }
 }
