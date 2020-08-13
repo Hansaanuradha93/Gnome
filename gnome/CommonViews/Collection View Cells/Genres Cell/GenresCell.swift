3c804b13c70126727b6b24cacd1ae312f743e835
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-13 22:30:55 +0530

    GenresCell refactored

--- gnome/CommonViews/Collection View Cells/Genres Cell/GenresCell.swift
+++ gnome/CommonViews/Collection View Cells/Genres Cell/GenresCell.swift
@@ -3,20 +3,19 @@ import UIKit
 class GenresCell: UICollectionViewCell {
     
     // MARK: Properties
-    static let reuseID      = "GenresCell"
-    private let titleLabel  = GNTitleLabel(fontSize: 20, textColour: .white, alignment: .center)
+    static let reuseID = "GenresCell"
+    private let titleLabel = GNTitleLabel(fontSize: 20, textColour: .white, alignment: .center)
     
     
     // MARK: Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         configureTitleLabel()
         configureBackground()
     }
     
     
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    required init?(coder: NSCoder) { fatalError() }
 }
 
 
@@ -27,14 +26,12 @@ extension GenresCell {
     
     
     private func configureBackground() {
-        
         backgroundColor = UIColor.appColor(.Pretty_Pink)
         addCornerRadius(of: 12)
     }
     
     
     private func configureTitleLabel() {
-        
         addSubview(titleLabel)
         
         NSLayoutConstraint.activate([
