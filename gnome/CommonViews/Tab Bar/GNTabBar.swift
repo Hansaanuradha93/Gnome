bffab1e1e76d7e369377ebe2fbdd38eb77011f05
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-13 22:12:53 +0530

    GNTabBar code style maintained

--- gnome/CommonViews/Tab Bar/GNTabBar.swift
+++ gnome/CommonViews/Tab Bar/GNTabBar.swift
@@ -2,12 +2,11 @@ import UIKit
 
 class GNTabBar: UITabBarController {
 
-    // MARK: - View Controller
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         UITabBar.appearance().tintColor = UIColor.appColor(.Pretty_Pink)
-        viewControllers                 = [createExploreNC(), createTrendingNC(), createSearchNC(), createLibraryNC(), createSettingsNC()]
+        viewControllers = [createExploreNC(), createTrendingNC(), createSearchNC(), createLibraryNC(), createSettingsNC()]
     }
 }
 
@@ -16,7 +15,6 @@ class GNTabBar: UITabBarController {
 extension GNTabBar {
     
     private func createExploreNC() -> UINavigationController {
-        
         let exploreVC = ExploreVC()
         exploreVC.title = Titles.explore
         exploreVC.tabBarItem = UITabBarItem(title: Titles.explore, image: Asserts.explore, tag: 0)
@@ -25,7 +23,6 @@ extension GNTabBar {
     
     
     private func createTrendingNC() -> UINavigationController {
-        
         let trendingVC = TrendingVC()
         trendingVC.title = Titles.trending
         trendingVC.tabBarItem = UITabBarItem(title: Titles.trending, image: Asserts.trending, tag: 1)
@@ -34,7 +31,6 @@ extension GNTabBar {
     
     
     private func createSearchNC() -> UINavigationController {
-        
         let searchVC = SearchVC()
         searchVC.title = Titles.search
         searchVC.tabBarItem = UITabBarItem(title: Titles.search, image: Asserts.search, tag: 2)
@@ -43,7 +39,6 @@ extension GNTabBar {
     
     
     private func createLibraryNC() -> UINavigationController {
-        
         let libraryVC = LibraryVC()
         libraryVC.title = Titles.library
         libraryVC.tabBarItem = UITabBarItem(title: Titles.library, image: Asserts.library, tag: 3)
@@ -52,7 +47,6 @@ extension GNTabBar {
     
     
     private func createSettingsNC() -> UINavigationController {
-        
         let settingsVC = SettingsVC()
         settingsVC.title = Titles.settings
         settingsVC.tabBarItem = UITabBarItem(title: Titles.settings, image: Asserts.settings, tag: 4)
