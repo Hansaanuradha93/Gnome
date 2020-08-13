3473f8a8bd9117a608943ed68be551774fa2731b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-13 22:14:40 +0530

    GNTitleLabel code refactored

--- gnome/CommonViews/Labels/GNTitleLabel.swift
+++ gnome/CommonViews/Labels/GNTitleLabel.swift
@@ -2,32 +2,32 @@ import UIKit
 
 class GNTitleLabel: UILabel {
     
-    // MARK: - Initializers
+    // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         configure()
     }
     
     
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    required init?(coder: NSCoder) { fatalError() }
     
     
     convenience init(fontSize: CGFloat, textColour: UIColor = .black, alignment: NSTextAlignment = .left) {
         self.init(frame: .zero)
-        
-        let traits          = [UIFontDescriptor.TraitKey.weight: UIFont.Weight.bold]
-        var descriptor      = UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.family: "Poppins"])
-        descriptor          = descriptor.addingAttributes([UIFontDescriptor.AttributeName.traits: traits])
-        font                = UIFont(descriptor: descriptor, size: fontSize)
-        textColor           = textColour
-        textAlignment       = alignment
+        let traits = [UIFontDescriptor.TraitKey.weight: UIFont.Weight.bold]
+        var descriptor = UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.family: "Poppins"])
+        descriptor = descriptor.addingAttributes([UIFontDescriptor.AttributeName.traits: traits])
+        font = UIFont(descriptor: descriptor, size: fontSize)
+        textColor = textColour
+        textAlignment = alignment
     }
+}
+
+
+// MARK: - Methods
+extension GNTitleLabel {
     
-    
-    // MARK: - Methods
     private func configure() {
-        
         adjustsFontSizeToFitWidth   = true
         minimumScaleFactor          = 0.9
         lineBreakMode               = .byTruncatingTail
