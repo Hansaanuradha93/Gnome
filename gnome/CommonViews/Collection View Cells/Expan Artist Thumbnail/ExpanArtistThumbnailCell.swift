f4000502dafe53ac7c970414396d4ac749d3f9af
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-15 23:46:57 +0530

    ExpanArtistThumbnailCell code cleared

--- gnome/CommonViews/Collection View Cells/Expan Artist Thumbnail/ExpanArtistThumbnailCell.swift
+++ gnome/CommonViews/Collection View Cells/Expan Artist Thumbnail/ExpanArtistThumbnailCell.swift
@@ -14,10 +14,6 @@ class ExpanArtistThumbnailCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureUI()
-//        configureThumbnailImage()
-//        setupNameLabel()
-//        configureTitleLabel()
-//        configurePlayButton()
     }
     
     
@@ -49,54 +45,5 @@ extension ExpanArtistThumbnailCell {
         titleLabel.anchor(top: nil, leading: thumbnailImageView.leadingAnchor, bottom: nameLabel.topAnchor, trailing: thumbnailImageView.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
         playButton.anchor(top: nil, leading: nil, bottom: nil, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 16), size: .init(width: dimensions, height: dimensions))
         playButton.centerYAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor).isActive = true
-        
     }
-    
-    
-//    private func configureThumbnailImage() {
-//        addSubview(thumbnailImageView)
-//
-//        NSLayoutConstraint.activate([
-//            thumbnailImageView.topAnchor.constraint(equalTo: topAnchor),
-//            thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
-//            thumbnailImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
-//            thumbnailImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -35)
-//        ])
-//    }
-    
-    
-//    private func configureTitleLabel() {
-//        titleLabel.alpha = 0.85
-//        thumbnailImageView.addSubview(titleLabel)
-//
-//        NSLayoutConstraint.activate([
-//            titleLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.leadingAnchor, constant: 16),
-//            titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: thumbnailImageView.trailingAnchor, constant: -16),
-//            titleLabel.bottomAnchor.constraint(equalTo: nameLabel.topAnchor)
-//        ])
-//    }
-    
-    
-//    private func setupNameLabel() {
-//        thumbnailImageView.addSubview(nameLabel)
-//
-//        NSLayoutConstraint.activate([
-//            nameLabel.leadingAnchor.constraint(lessThanOrEqualTo: thumbnailImageView.leadingAnchor, constant: 16),
-//            nameLabel.trailingAnchor.constraint(lessThanOrEqualTo: thumbnailImageView.trailingAnchor, constant: -16),
-//            nameLabel.bottomAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: -32)
-//        ])
-//    }
-    
-    
-//    private func configurePlayButton() {
-//        let dimensions: CGFloat = 70
-//        addSubview(playButton)
-//
-//        NSLayoutConstraint.activate([
-//            playButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-//            playButton.centerYAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor),
-//            playButton.widthAnchor.constraint(equalToConstant: dimensions),
-//            playButton.heightAnchor.constraint(equalToConstant: dimensions)
-//        ])
-//    }
 }
