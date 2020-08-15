e562534aca8eeb0fd56ea468d11045a0bbbe6854
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-16 00:25:21 +0530

    SongCell refactored

--- gnome/CommonViews/Collection View Cells/Song Cell/SongCell.swift
+++ gnome/CommonViews/Collection View Cells/Song Cell/SongCell.swift
@@ -11,7 +11,7 @@ class SongCell: UICollectionViewCell {
     // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+        configureUI()
     }
     
     
@@ -37,21 +37,10 @@ extension SongCell {
     }
     
     
-    private func configure() {
+    private func configureUI() {
         addSubview(thumbnailImageView)
         addSubview(titleLabel)
         
-        NSLayoutConstraint.activate([
-            thumbnailImageView.topAnchor.constraint(equalTo: topAnchor),
-            thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            thumbnailImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            thumbnailImageView.heightAnchor.constraint(equalTo: thumbnailImageView.widthAnchor),
-            
-            titleLabel.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 4.7),
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
-            
-        ])
-    }
+        thumbnailImageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, size: .init(width: 0, height: self.frame.width))
+        titleLabel.anchor(top: thumbnailImageView.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 4.7, left: 0, bottom: 0, right: 0))    }
 }
