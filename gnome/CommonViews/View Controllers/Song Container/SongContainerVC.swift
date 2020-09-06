a520b6a4b10fb00912e1f68ab6f95237a43d1d7c
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-06 22:10:29 +0530

    SongContainerVC code refactored

--- gnome/CommonViews/View Controllers/Song Container/SongContainerVC.swift
+++ gnome/CommonViews/View Controllers/Song Container/SongContainerVC.swift
@@ -33,17 +33,17 @@ class SongContainerVC: UIViewController {
     // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureAlbumTitleLabel()
-        configureThumbnailImageView()
-        configureSongTitleLabel()
-        configureArtistLabel()
+        configureUI()
+//        configureAlbumTitleLabel()
+//        configureThumbnailImageView()
+//        configureSongTitleLabel()
+//        configureArtistLabel()
         configurePlayerContainerVC()
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        configureViewController()
         setupUI(with: songs[index])
     }
 }
@@ -52,7 +52,9 @@ class SongContainerVC: UIViewController {
 // MARK: - Methods
 extension SongContainerVC {
     
-    private func configurePlayerContainerVC() { playerContainerVC.playerContainerDelegate = self }
+    private func configurePlayerContainerVC() {
+        playerContainerVC.playerContainerDelegate = self
+    }
     
     
     private func setupUI(with song: Song?) {
@@ -63,54 +65,71 @@ extension SongContainerVC {
         artistLabel.text = song.artist
     }
     
-    private func configureViewController() {
+    private func configureUI() {
         view.backgroundColor = .systemBackground
         navigationController?.setNavigationBarHidden(true, animated: true)
-    }
-    
-    
-    private func configureAlbumTitleLabel() {
+        
         view.addSubview(albumTitleLabel)
-        
-        NSLayoutConstraint.activate([
-            albumTitleLabel.topAnchor.constraint(equalTo: view.topAnchor),
-            albumTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-    }
-    
-    
-    private func configureThumbnailImageView() {
-        let padding: CGFloat = 29
         view.addSubview(thumbnailImageView)
-        
-        NSLayoutConstraint.activate([
-            thumbnailImageView.topAnchor.constraint(equalTo: albumTitleLabel.bottomAnchor, constant: padding),
-            thumbnailImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            thumbnailImageView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            thumbnailImageView.heightAnchor.constraint(equalTo: view.widthAnchor)
-        ])
-    }
-    
-    
-    private func configureSongTitleLabel() {
         view.addSubview(songTitleLabel)
-        
-        NSLayoutConstraint.activate([
-            songTitleLabel.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 30),
-            songTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-    }
-    
-    
-    private func configureArtistLabel() {
         view.addSubview(artistLabel)
-        artistLabel.alpha = 0.6
         
-        NSLayoutConstraint.activate([
-            artistLabel.topAnchor.constraint(equalTo: songTitleLabel.bottomAnchor, constant: 3),
-            artistLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
+        artistLabel.alpha = 0.6
+        let dimensions: CGFloat = view.frame.width - 60
+                
+        albumTitleLabel.anchor(top: view.topAnchor, leading: nil, bottom: nil, trailing: nil)
+        albumTitleLabel.centerHorizontallyInSuperView()
+        thumbnailImageView.anchor(top: albumTitleLabel.bottomAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(top: 29, left: 0, bottom: 0, right: 0))
+        thumbnailImageView.centerHorizontallyInSuperView(size: .init(width: dimensions, height: dimensions))
+        songTitleLabel.anchor(top: thumbnailImageView.bottomAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(top: 30, left: 0, bottom: 0, right: 0))
+        songTitleLabel.centerHorizontallyInSuperView()
+        artistLabel.anchor(top: songTitleLabel.bottomAnchor, leading: nil, bottom: nil, trailing: nil, padding: .init(top: 3, left: 0, bottom: 0, right: 0))
+        artistLabel.centerHorizontallyInSuperView()
     }
+    
+    
+//    private func configureAlbumTitleLabel() {
+//        view.addSubview(albumTitleLabel)
+//
+//        NSLayoutConstraint.activate([
+//            albumTitleLabel.topAnchor.constraint(equalTo: view.topAnchor),
+//            albumTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+//        ])
+//    }
+    
+    
+//    private func configureThumbnailImageView() {
+//        let padding: CGFloat = 29
+//        view.addSubview(thumbnailImageView)
+//
+//        NSLayoutConstraint.activate([
+//            thumbnailImageView.topAnchor.constraint(equalTo: albumTitleLabel.bottomAnchor, constant: padding),
+//            thumbnailImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            thumbnailImageView.widthAnchor.constraint(equalTo: view.widthAnchor),
+//            thumbnailImageView.heightAnchor.constraint(equalTo: view.widthAnchor)
+//        ])
+//    }
+    
+    
+//    private func configureSongTitleLabel() {
+//        view.addSubview(songTitleLabel)
+//
+//        NSLayoutConstraint.activate([
+//            songTitleLabel.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 30),
+//            songTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+//        ])
+//    }
+    
+    
+//    private func configureArtistLabel() {
+//        view.addSubview(artistLabel)
+//        artistLabel.alpha = 0.6
+//
+//        NSLayoutConstraint.activate([
+//            artistLabel.topAnchor.constraint(equalTo: songTitleLabel.bottomAnchor, constant: 3),
+//            artistLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+//        ])
+//    }
 }
 
 
