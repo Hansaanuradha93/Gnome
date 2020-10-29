a015abc6d23ea9092afad14f7c2d225c284d4427
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-29 22:24:42 +0530

    Slider code cleaned

--- gnome/CommonViews/Slider/GNSlider.swift
+++ gnome/CommonViews/Slider/GNSlider.swift
@@ -2,12 +2,14 @@ import UIKit
 
 class GNSlider: UISlider {
     
+    // MARK: Initializers
     override init(frame: CGRect) { super.init(frame: frame) }
     
     
     required init?(coder: NSCoder) { fatalError() }
    
-    
+   
+    // MARK: Override Methods
     override func trackRect(forBounds bounds: CGRect) -> CGRect {
         let customBounds = CGRect(origin: bounds.origin, size: CGSize(width: bounds.size.width, height: 5.0))
         super.trackRect(forBounds: customBounds)
